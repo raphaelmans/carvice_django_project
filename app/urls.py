70d7cca5a387481df0565b9fcbc5f85bfe9dc568
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2021-09-07 14:40:29 +0800

    added html

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('aboutus', views.AboutUsView.as_view(), name='about_us'),
+    path('about', views.AboutUsView.as_view(), name='aboutus'),
+    path('features', views.FeaturesView.as_view(), name='features'),
+    path('contact', views.ContactView.as_view(), name='contact'),
+    path('signup', views.SignUpView.as_view(), name='sign_up'),
+    path('signin', views.SignInView.as_view(), name='signin'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -10,3 +10,28 @@ class AboutUsView(View):
     
     def get(self,request):
         return render(request, 'pages/about_us.html',{})
+
+
+class FeaturesView(View):
+    
+    def get(self,request):
+        return render(request, 'pages/features.html',{})
+
+        
+class ContactView(View):
+    
+    def get(self,request):
+        return render(request, 'pages/contact.html',{})
+
+class SignUpView(View):
+    
+    def get(self,request):
+        return render(request, 'pages/signup.html',{})
+
+
+class SignInView(View):
+    
+    def get(self,request):
+        return render(request, 'pages/signin.html',{})
+
+
