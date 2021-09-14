3b0fb5220ed92d7ecdca8815eb40cbe7b228f403
Raphael Mansueto <raphaelmanswork@gmail.com>
2021-09-14 12:45:43 +0800

    Removed previous app

--- cv/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-# Create your views here.
-
-def index(request):
-    return render(request,'cv/index.html')
-
-
