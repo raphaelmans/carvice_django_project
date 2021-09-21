4e74a4c51e329ef2b2f5514d13f3172afa6de9e8
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2021-09-21 14:28:01 +0800

    add car, rental, user inserts

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+app_name = 'app'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
@@ -10,6 +11,8 @@ urlpatterns = [
     path('signup', views.SignUpView.as_view(), name='sign_up'),
     path('signin', views.SignInView.as_view(), name='signin'),
     path('dashboard', views.DashboardView.as_view(), name='dashboard'),
-
+    path('userregistration', views.UserRegistrationView.as_view(), name='user_registration_view'),
+    path('carregistration', views.CarRegistrationView.as_view(), name='car_registration_view'),
+    path('rentalcarregistration', views.RentalCarRegistrationView.as_view(), name='rentalcar_registration_view'),
 
 ]
--- carvice/urls.py
+++ carvice/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+app_name = 'app'
+
+
 urlpatterns = [
     path('',include('app.urls')),
     path('admin/', admin.site.urls),
